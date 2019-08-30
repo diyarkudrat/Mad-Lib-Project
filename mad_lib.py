8a4e44917f2230a8d939e8f89614a202199f221d
DIYAR KUDRAT <dkudrat@gmail.com>
2019-08-30 14:14:05 -0700

    added the story layout in the print_story_out function

--- mad_lib.py
+++ mad_lib.py
@@ -9,7 +9,7 @@
 # print("It's been a " + adj + " summer here in " + noun + " City. The " + adj
 #     + " car has been " + ing_verb + " the whole season. I think it's time to " + verb + " the car.")
 
-words = ('adjective','noun','adj','verb ending in -ing','verb')
+words = ('adjective','noun','adj','verb ending in -ing','verb','adjective','adjective','number','name')
 
 def is_input_valid(input_of_list):
     if input_of_list == "":
@@ -32,24 +32,27 @@ def create_story(input):
                 invalid_input = False
     return stored_list
 
-
-
-
-
-
-
-
-
-
-def select(function_code):
+def print_story_out(function_code, given_word):
     if function_code == 'S':
-        start_game = input("Press S to start game!")
-        fun_game()
+        print("It's been a " + given_word[1] + " summer here in " + given_word[2] + " City. The " +
+        given_word[3] + " car has been " + given_word[4] + " the whole season. I think it's time to " +
+        given_word[5] + " the car and buy a new one. The new car should be " + given_word[6] + " and " +
+        given_word[7] + " and worth " + given_word[8] + ". The new car's name will be " + given_word[9] + ".")
+    else:
+        print("Nothing selected")
 
-def mad_lib_game():
-    running = True
 
-    while running:
-        selection = input("Press S to start the game!")
-        running = enter_words(selection)
-mad_lib_game()
+
+
+    # def select(function_code):
+    #     if function_code == 'S':
+    #         start_game = input("Press S to start game!")
+    #         fun_game()
+
+# def mad_lib_game():
+#     running = True
+#
+#     while running:
+#         selection = input("Press S to start the game!")
+#         running = enter_words(selection)
+# mad_lib_game()
